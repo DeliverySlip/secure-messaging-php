cbb0702df8ca96792b887a8e447bf2481e3de864
bensoer <ben@soernet.ca>
2017-01-08 21:42:45 -0800

    created guzzleclientsingleton to keep same instance settings accross SDK

---
+++ lib/lib/GuzzleClientSingleton.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bensoer
+ * Date: 08/01/17
+ * Time: 9:25 PM
+ */
+
+namespace SecureMessaging\Lib;
+
+
+use GuzzleHttp\Client;
+
+final class GuzzleClientSingleton
+{
+    private static $instance = null;
+
+    public static function getInstance(){
+        if(self::$instance == null){
+            self::$instance = new Client([
+                "exceptions" => false
+            ]);
+        }
+
+        return self::$instance;
+    }
+
+
+    private final function __construct(){}
+
+    public final function __destruct(){
+        unset(self::$instance);
+    }
+}
